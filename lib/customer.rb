95145a4de00b99bac89f0d1aef8c6b40f648da2f
Sıtkı Bağdat <sbagdat@gmail.com>
2016-04-07 22:40:22 +0300

    Add ability to create and count customers Add ability to detect duplicates Add ability to find a customer by name

---
+++ lib/customer.rb
@@ -0,0 +1,32 @@
+class Customer
+  @@customers = []
+
+  attr_reader :name
+
+  def initialize(options = {})
+    @name = options[:name] || 'No name'
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(customer_name)
+    @@customers.each do |customer|
+      return customer if customer.name == customer_name
+    end
+    nil # not found
+  end
+
+  private
+
+  def exist?
+    @@customers.any? { |c| c.name == @name }
+  end
+
+  def add_to_customers
+    raise DuplicateProductError, "#{@name} already exists." if exist?
+    @@customers << self
+  end
+end
