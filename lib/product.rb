d0a1d1ac0326abfea59afd785fd8b705ef662159
Sıtkı Bağdat <sbagdat@gmail.com>
2016-04-07 19:01:52 +0300

    Add ability to prevent for saving same product twice It will raises DuplicateProductError

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,24 @@
 class Product
   @@products = []
+  attr_reader :title
 
   def initialize(options={})
-  	@@products << self
+  	@title = options[:title]
+  	add_to_products
   end
 
   def self.all
     @@products
   end
+
+  private 
+
+  def add_to_products
+  	raise DuplicateProductError, "#{@title} already exists." if exist?
+  	@@products << self
+  end
+
+  def exist?
+  	 @@products.any? { |p| p.title == @title }
+  end	
 end
